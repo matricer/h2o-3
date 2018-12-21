fc1074126c814ca8f177dd11136235a202345c1b
Michal Raška <michal.raska@gmail.com>
2018-12-21 16:03:06 +0100

    Create missing dir.

--- scripts/jenkins/groovy/mojo2Stage.groovy
+++ scripts/jenkins/groovy/mojo2Stage.groovy
@@ -3,7 +3,10 @@ def call(final pipelineContext, final stageConfig) {
         error "customData.mojoDaiVersion must be set."
     }
     withCredentials([string(credentialsId: "DRIVERLESS_AI_LICENSE_KEY", variable: "DRIVERLESS_AI_LICENSE_KEY")]) {
-        sh "echo ${DRIVERLESS_AI_LICENSE_KEY} > ${WORKSPACE}/${stageConfig.stageDir}/license.sig"
+        sh """
+            mkdir -p ${WORKSPACE}/${stageConfig.stageDir}
+            echo ${DRIVERLESS_AI_LICENSE_KEY} > ${WORKSPACE}/${stageConfig.stageDir}/license.sig
+        """
         try {
             withEnv(["DRIVERLESS_AI_LICENSE_FILE=${WORKSPACE}/${stageConfig.stageDir}/license.sig", "MOJO_PIPELINE_TEST_DIR=/home/0xdiag/migration_mojo/${stageConfig.customData.mojoDaiVersion}/test_mojo_package"]) {
                 def defaultStage = load('h2o-3/scripts/jenkins/groovy/defaultStage.groovy')
