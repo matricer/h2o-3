35e9c0a30e3ee2d7dd1068caa4ae121023c34a8d
Michal Raška <michal.raska@gmail.com>
2018-12-21 15:19:28 +0100

    Fix typo.

--- scripts/jenkins/groovy/mojo2Stage.groovy
+++ scripts/jenkins/groovy/mojo2Stage.groovy
@@ -2,7 +2,7 @@ def call(final pipelineContext, final stageConfig) {
     if (!stageConfig.customData.mojoDaiVersion) {
         error "customData.mojoDaiVersion must be set."
     }
-    withCredentials([string(credentialsId: "DRIVERLESS_AI_LICENSE_KEY", variable: "DRIVERLESS_AI_LICENSE_KEY"]) {
+    withCredentials([string(credentialsId: "DRIVERLESS_AI_LICENSE_KEY", variable: "DRIVERLESS_AI_LICENSE_KEY")]) {
         sh "echo ${DRIVERLESS_AI_LICENSE_KEY} > ${WORKSPACE}/${stageConfig.stageDir}/license.sig"
         try {
             withEnv(["DRIVERLESS_AI_LICENSE_FILE=${WORKSPACE}/${stageConfig.stageDir}/license.sig", "MOJO_PIPELINE_TEST_DIR=/home/0xdiag/migration_mojo/${stageConfig.customData.mojoDaiVersion}/test_mojo_package"]) {
